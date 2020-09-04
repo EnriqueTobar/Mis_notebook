ac2274ee9f9b7428bc273a82632c061b0113b4e7
EnriqueTobar <64887001+EnriqueTobar@users.noreply.github.com>
2020-09-04 00:20:55 -0400

    Creación de Carpeta SQL

---
+++ SQL/hackerRank.sql
@@ -0,0 +1,32 @@
+/* Query the list of CITY names from STATION which have vowels (i.e., a, e, i, o, and u) 
+as both their first and last characters. Your result cannot contain duplicates. */
+
+select distinct city
+from station
+where left(city,1) in ('a','e','i','o','u')
+    and right(city, 1) in ('a','e','i','o','u')
+
+/* Query the Name of any student in STUDENTS who scored higher than  Marks. 
+Order your output by the last three characters of each name. If two or more 
+students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), 
+secondary sort them by ascending ID. */
+
+SELECT NAME
+FROM STUDENTS
+WHERE MARKS > 75
+ORDER BY RIGHT(NAME,3),ID ASC
+
+/* Caso de los Triangulos */
+
+SELECT CASE
+            WHEN A + B > C AND B + C > A AND A + C > B THEN
+                CASE 
+                    WHEN A = B AND B = C THEN 'Equilateral'
+                    WHEN A = B OR B = C OR A = C THEN 'Isosceles'
+                    ELSE 'Scalene'
+                END
+            ELSE 'Not A Triangle'
+        END
+FROM TRIANGLES;
+
+
